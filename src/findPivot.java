a6c2ca1090a413a496850f787cd4f1ac27e8f8df
Anil <anilmende2002@email.com>
2025-12-25 15:55:39 +0530

    finding pivot

---
+++ src/findPivot.java
@@ -0,0 +1,30 @@
+public class findPivot {
+    public static void main(String[] args) {
+        int nums[] = {4,5,6,0,1,2,3};
+        System.out.println("The Pivot element is " + pivot(nums));
+    }
+
+    static int pivot(int nums[]){
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(mid < end && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            if(mid > start && nums[mid] < nums[mid-1]){
+                return mid-1;
+            }
+
+            if(nums[mid] <= nums[start]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
