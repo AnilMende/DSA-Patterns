765e186fda45af3359f9e859b3bcc738f2621618
Anil <anilmende2002@email.com>
2026-01-19 20:30:21 +0530

    Exactly K Pattern

--- src/VariableSizeExactlyK.java
+++ src/VariableSizeExactlyK.java
@@ -12,7 +12,13 @@ public class VariableSizeExactlyK {
         //Problem 2
         int nums1[] = {1,3,1,2,2};
         int res1 = CountCompleteSubarrays(nums1,3) - CountCompleteSubarrays(nums1,2);
-        System.out.println(res1);
+        System.out.println("Count of Complete Subarrays are " + res1);
+
+        //Problem 3
+        int nums2[] = {1,0,1,0,1};
+        int goal = 2;
+        int res2 = numSubarraysWithSum(nums2, goal) - numSubarraysWithSum(nums2,goal-1);
+        System.out.println("Number of Subarrays with sum " + res2);
     }
 
     static int atMost(int nums[], int k){
@@ -57,4 +63,30 @@ public class VariableSizeExactlyK {
         }
         return count;
     }
+
+    // Binary subarrays with sum
+    //return the number of non-empty subarrays with sum goal
+
+    static int numSubarraysWithSum(int nums[], int k){
+        if(k < 0){
+            return 0;
+        }
+
+        int count2 = 0;
+        int sum2 = 0;
+        int left = 0;
+
+        for(int right = 0;right < nums.length;right++){
+            sum2 += nums[right];
+
+            while(sum2 > k){
+                sum2 -= nums[left];
+                left++;
+            }
+
+            count2 += right - left + 1;
+        }
+        return count2;
+    }
+
 }
